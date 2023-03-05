f17cdf968dbd25e85c29d2074ddea7c3dfa69d75
Georgi_Dodekov <112122248+djidjii@users.noreply.github.com>
2023-03-05 16:04:19 +0200

    Update account.py

--- classes and objects/account.py
+++ classes and objects/account.py
@@ -1,22 +1,25 @@
 class Account:
-    def __int__(self, id: int, name: str, balance: int):
+
+    def __init__(self, id, name, balance=0):
         self.id = id
         self.name = name
         self.balance = balance
 
-    def credit(self, amount) -> str:
+    def credit(self, amount):
         self.balance += amount
-        return f"{self.balance}"
+        return self.balance
 
-    def debit(self, amount) -> str:
+    def debit(self, amount):
         if amount <= self.balance:
-            self.balance - amount
-            return f"{self.balance}"
-
-        else:
-            return f"Amount exceeded balance"
+            self.balance -= amount
+            return self.balance
+        return "Amount exceeded balance"
 
     def info(self):
-        return f"User {self.name} with account {id} has {self.balance} balance"
-
-
+        return f"User {self.name} with account {self.id} has {self.balance} balance"
+    
+    
+account = Account(1234, "George", 1000)
+print(account.credit(500))
+print(account.debit(1500))
+print(account.info())
