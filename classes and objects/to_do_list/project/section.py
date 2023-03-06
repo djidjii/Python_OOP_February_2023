1454af650f630686444ca690624052d6fee000ce
Georgi_Dodekov <112122248+djidjii@users.noreply.github.com>
2023-03-06 21:14:01 +0200

    Add files via upload

---
+++ classes and objects/to_do_list/project/section.py
@@ -0,0 +1,34 @@
+from project.task import Task
+
+
+class Section:
+
+    def __init__(self, name):
+        self.name = name
+        self.tasks = {}
+
+    def add_task(self, new_task: Task):
+        if new_task.name not in self.tasks.keys():
+            self.tasks[new_task.name] = new_task
+            return f"Task {new_task.details()} is added to the section"
+        return f"Task is already in the section {self.name}"
+
+    def complete_task(self, task_name):
+        if task_name in self.tasks.keys():
+            self.tasks[task_name].completed = True
+            return f"Completed task {task_name}"
+        return f"Could not find task with the name {task_name}"
+
+    def clean_section(self):
+        deleted_tasks_count = 0
+        task_keys = self.tasks.copy().keys()
+        for task_key in task_keys:
+            if self.tasks[task_key].completed:
+                deleted_tasks_count += 1
+                del self.tasks[task_key]
+
+        return f"Cleared {deleted_tasks_count} tasks."
+
+    def view_section(self):
+        show_tasks_format = '\n'.join([task.details() for task in self.tasks.values()])
+        return f"Section {self.name}:\n{show_tasks_format}"
