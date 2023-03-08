02fb17c09c3cac8c266507caed5a1144c8d5b9db
Georgi_Dodekov <112122248+djidjii@users.noreply.github.com>
2023-03-08 23:16:57 +0200

    Delete inheritance_exercise/zoo/project/player_and_monsters/project directory

--- inheritance_exercise/zoo/project/player_and_monsters/project/hero.py
+++
@@ -1,7 +0,0 @@
-class Hero:
-    def __int__(self, username, level):
-        self.username = username
-        self.level = level
-
-    def __str__(self) -> str:
-        return f"{self.username} of type {self.__class__.__name__} has level {self.level}"
